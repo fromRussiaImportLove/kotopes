c3f0ae2cbc14584a3d8a739f4f9c89be84cbbda9
Muffin Knight <muffin@lukojo.com>
2020-11-21 20:47:26 +0300

    add pandas calculate

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (PaymentsViewSet, AnimalsViewSet,
-                       TransactionsViewSet, import_payments_data)
+                       TransactionsViewSet, import_payments_data,
+                       calculate_history, calculate_newdata)
 
 router_v1 = DefaultRouter()
 router_v1.register('payments', PaymentsViewSet,
@@ -14,5 +15,7 @@ router_v1.register('transactions', TransactionsViewSet,
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/import_payments_data', import_payments_data)
+    path('v1/import_payments_data', import_payments_data),
+    path('v1/history_data', calculate_history),
+    path('v1/calculate_newdata', calculate_newdata)
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,13 +6,18 @@ from rest_framework.response import Response
 from api.serializers import PaymentsSerializer, AnimalsSerializer, TransactionsSerializer
 from api.models import Payments, Animals, Transactions
 from csv_import.import_data import import_payments
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from utils.pandas_calculate import fcalc, ncalc
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
 
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
+    # lookup_field = 'shelter'
+    search_fields = ['=user_id', '=shelter']
+    filter_backends = [filters.SearchFilter]
 
 
 class AnimalsViewSet(viewsets.ModelViewSet):
@@ -29,4 +34,18 @@ class TransactionsViewSet(viewsets.ModelViewSet):
 
 def import_payments_data(request):
     result = import_payments()
+    return HttpResponse(result)
+
+
+def calculate_history(request):
+    queryset = Payments.objects.values('user_id', 'shelter', 'datetime', 'item', 'amount')
+    result = fcalc(queryset)
+    #return JsonResponse(result)
+    return HttpResponse(result)
+
+
+def calculate_newdata(request):
+    min_price = int(request.POST['min_price'])
+    n_sales = int(request.POST['n_sales'])
+    result = ncalc(min_price, n_sales)
     return HttpResponse(result)
