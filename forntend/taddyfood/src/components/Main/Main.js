7b579c0da047039e4604a2663862455afbae6693
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-21 14:57:11 +0300

    add Main component

---
+++ forntend/taddyfood/src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+    return (
+        <section className="main">
+            <div className="main__container">
+                <p className="main__description">Что то о метриках</p>
+                <div className="main__cards"></div>
+            </div>
+        </section>
+    )
+}
+
+export default Main;
