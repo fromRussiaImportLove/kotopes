4a6b37e551f74bb5c16a07eca8d2197451aaefd3
Nick Lobanov <nk.nicolas142@gmail.com>
2020-11-21 14:46:32 +0300

    cra and create header component

---
+++ forntend/taddyfood/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <h1 className="header__logo">Taddy Food</h1>
+            </div>  
+        </header>
+    )
+}
+
+export default Header;
