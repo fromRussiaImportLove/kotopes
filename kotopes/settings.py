59b3e3424b6bbf77deddb489d22deb5cb09c0599
Muffin Knight <muffin@lukojo.com>
2020-11-22 13:30:15 +0300

    new dir for back static

--- kotopes/settings.py
+++ kotopes/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_back/'
 
 
 #RESTapi
